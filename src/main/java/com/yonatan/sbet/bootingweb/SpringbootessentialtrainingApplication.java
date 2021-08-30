80d13e5146f1870e96fc663b9854f964d425c502
yaldaag <34406716+yaldaag@users.noreply.github.com>
2021-08-30 16:57:26 +0300

    Initial commit

---
+++ src/main/java/com/yonatan/sbet/bootingweb/SpringbootessentialtrainingApplication.java
@@ -0,0 +1,13 @@
+package com.yonatan.sbet.bootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootessentialtrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootessentialtrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yonatan/sbet/bootingweb/SpringbootessentialtrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonatan.sbet.bootingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootessentialtrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
